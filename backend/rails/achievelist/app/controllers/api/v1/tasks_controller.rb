e344be4b8954bd7432e01bb42ab43989ca65ab65
harupyon <naruha.tomone@gmail.com>
2023-03-12 18:43:27 +0900

    バックエンド＞タスク削除API実装、全APIの返却値にステータスコードを追加

--- backend/rails/achievelist/app/controllers/api/v1/tasks_controller.rb
+++ backend/rails/achievelist/app/controllers/api/v1/tasks_controller.rb
@@ -8,15 +8,16 @@ module Api
         # TODO: JWTから取得
         user_id = '1'
 
-        if cleared == true
-          result = ClearedTask.where(user_id)
-        elsif cleared == false
-          result = Task.where(user_id)
-        else
-          ResponseGenerator.validation_error(['cleaed は真偽値で指定してください。'])
-        end
+        result =
+          if cleared == true
+            ResponseGenerator.success(ClearedTask.where(user_id))
+          elsif cleared == false
+            ResponseGenerator.success(Task.where(user_id))
+          else
+            ResponseGenerator.validation_error(['cleaed は真偽値で指定してください。'])
+          end
 
-        render json: result
+        render json: result.fetch(:body), status: result.fetch(:status)
       end
 
       # タスク保存API
@@ -39,7 +40,27 @@ module Api
             ResponseGenerator.success
           end
 
-        render json: result unless result.nil?
+        render json: result.fetch(:body), status: result.fetch(:status) unless result.nil?
+      end
+
+      # タスク削除API
+      def destroy
+        task_id = params[:id]
+
+        # TODO: JWTから取得
+        user_id = '1'
+
+        task = Task.find_by(id: task_id, user_id:)
+        result =
+          if task.nil?
+            ResponseGenerator.not_found
+          elsif !task.destroy
+            ResponseGenerator.db_error
+          else
+            ResponseGenerator.success
+          end
+
+        render json: result.fetch(:body), status: result.fetch(:status) unless result.nil?
       end
     end
   end
--- backend/rails/achievelist/app/controllers/concerns/response_generator.rb
+++ backend/rails/achievelist/app/controllers/concerns/response_generator.rb
@@ -4,27 +4,48 @@ module ResponseGenerator
   def success(body = nil)
     return if body.nil?
 
-    body
+    {
+      body:,
+      status: 200
+    }
   end
 
   def validation_error(errors = [])
     {
-      'title': 'params not validated.',
-      'invalid-params': errors
+      body: {
+        'title': 'params not validated.',
+        'invalid-params': errors
+      },
+      status: 400
     }
   end
 
   def un_authorized_error
     {
-      'title': 'unauthorized.'
+      body: {
+        'title': 'unauthorized.'
+      },
+      status: 401
+    }
+  end
+
+  def not_found
+    {
+      body: {
+        'title': 'resource not found.'
+      },
+      status: 404
     }
   end
 
   def db_error
     {
-      'title': 'db operations failed.'
+      body: {
+        'title': 'db operations failed.'
+      },
+      status: 500
     }
   end
 
-  module_function :success, :validation_error, :un_authorized_error, :db_error
+  module_function :success, :validation_error, :un_authorized_error, :not_found, :db_error
 end
