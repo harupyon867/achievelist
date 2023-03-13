23ef57a1204f769f85fbe6f74e211ef26d15ec28
harupyon <naruha.tomone@gmail.com>
2023-03-13 21:53:52 +0900

    バックエンド＞タスク更新API 実装

--- backend/rails/achievelist/app/controllers/api/v1/tasks_controller.rb
+++ backend/rails/achievelist/app/controllers/api/v1/tasks_controller.rb
@@ -62,6 +62,32 @@ module Api
 
         render json: result.fetch(:body), status: result.fetch(:status) unless result.nil?
       end
+
+      # タスク更新API
+      def update
+        task_id = params[:id]
+        updates = params.permit(:name, :limit, :priority)
+
+        # TODO: JWTから取得
+        user_id = '1'
+
+        task = Task.find_by(id: task_id, user_id:)
+
+        if task.nil?
+          result = ResponseGenerator.not_found
+        else
+          task.assign_attributes(updates)
+          result =
+            if !task.valid?
+              ResponseGenerator.validation_error(task.errors.full_messages)
+            elsif !task.save
+              ResponseGenerator.db_error
+            else
+              ResponseGenerator.success
+            end
+        end
+        render json: result.fetch(:body), status: result.fetch(:status) unless result.nil?
+      end
     end
   end
 end
